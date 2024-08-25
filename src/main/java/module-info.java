942500c914a282a2e7c6d5aacae667e75a45f8ab
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2024-08-25 15:26:58 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module poo.uniquindio.edu.co.demo {
+
+
     requires javafx.controls;
     requires javafx.fxml;
 
 
     opens poo.uniquindio.edu.co.demo to javafx.fxml;
     exports poo.uniquindio.edu.co.demo;
+
+    opens poo.uniquindio.edu.co.demo.Controller;
+    exports poo.uniquindio.edu.co.demo.Controller;
+
+    opens poo.uniquindio.edu.co.demo.Model;
+    exports poo.uniquindio.edu.co.demo.Model;
 }
