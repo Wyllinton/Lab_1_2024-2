4fe13d23920bf80f007ebf3917f476bf54801c2d
DanielJNarvaezH <danielj.narvaezh@uqvirtual.edu.co>
2024-08-24 22:33:08 -0500

    Contralor club deportivo listo

--- src/main/java/poo/uniquindio/edu/co/demo/Controller/AdministradorController.java
+++ src/main/java/poo/uniquindio/edu/co/demo/Controller/AdministradorController.java
@@ -1,56 +1,6 @@
 package poo.uniquindio.edu.co.demo.Controller;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-import poo.uniquindio.edu.co.demo.App;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-
-public class AdministradorController implements Initializable{
-        @FXML
-    private ComboBox<TipoOperacion> comb;
-
-    @FXML
-    void Seleccionar(ActionEvent event) throws IOException {
-        TipoOperacion operacionSeleccionada = comb.getSelectionModel().getSelectedItem();
-
-        switch (operacionSeleccionada) {
-            case PERMUTACION:
-                mostrarVentana("Permutacion");        
-                break;
-            case VARIACIONES:
-                mostrarVentana("Variaciones");
-                break;
-            case COMBINATORIA:
-                mostrarVentana("Combinatoria");
-                break;
-            default:
-                System.out.println("Opción no válida.");
-        }
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<TipoOperacion> tiposOperacion = FXCollections.observableArrayList(TipoOperacion.values());
-        comb.setItems(tiposOperacion);
-        comb.getSelectionModel();
-    }
-
-    private void mostrarVentana(String tituloVentana) throws IOException {
-        App.setRoot(tituloVentana);
-    }
-    
-
-    public enum TipoOperacion {
-        PERMUTACION,
-        VARIACIONES,
-        COMBINATORIA
-    }
-
+public class AdministradorController {
+      
 }
--- src/main/java/poo/uniquindio/edu/co/demo/Controller/ClubDeportivoController.java
+++ src/main/java/poo/uniquindio/edu/co/demo/Controller/ClubDeportivoController.java
@@ -1,5 +1,57 @@
 package poo.uniquindio.edu.co.demo.Controller;
 
-public class ClubDeportivoController {
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import java.net.URL;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import poo.uniquindio.edu.co.demo.App;
+
+public class ClubDeportivoController implements Initializable {
+
+    @FXML
+    private ComboBox<TipoGestion> comb;
+
+    @FXML
+    void Seleccionar(ActionEvent event) throws IOException {
+        TipoGestion operacionSeleccionada = comb.getSelectionModel().getSelectedItem();
+
+        switch (operacionSeleccionada) {
+            case ADMINISTRADOR:
+                mostrarVentana("Administrador-view");        
+                break;
+            case ENTRENADOR:
+                mostrarVentana("Entrenador-view");
+                break;
+            case MIEMBRO:
+                mostrarVentana("Miembro-view");
+                break;
+            default:
+                System.out.println("Opción no válida.");
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<TipoGestion> tiposOperacion = FXCollections.observableArrayList(TipoGestion.values());
+        comb.setItems(tiposOperacion);
+        comb.getSelectionModel();
+    }
+
+    private void mostrarVentana(String tituloVentana) throws IOException {
+        App.setRoot(tituloVentana);
+    }
     
+
+    public enum TipoGestion {
+        ADMINISTRADOR,
+        ENTRENADOR,
+        MIEMBRO
+    }
+
 }
