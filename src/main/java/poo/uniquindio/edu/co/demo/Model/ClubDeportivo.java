da4e39eb0ec781914a98825ea573c2195c294566
Willinton Vergara Cataño <willinton.vergarac@uqvirtual.edu.co>
2024-08-31 23:01:41 -0500

    Refactorización Código ClubDeportivo

--- src/main/java/poo/uniquindio/edu/co/demo/Model/ClubDeportivo.java
+++ src/main/java/poo/uniquindio/edu/co/demo/Model/ClubDeportivo.java
@@ -2,12 +2,8 @@ package poo.uniquindio.edu.co.demo.Model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
 
-import util.AssertionUtil;
-
-public class ClubDeportivo implements gestionarMiembro {
+public class ClubDeportivo{
 
     private static ClubDeportivo instancia; // Atributo que almacena la única instancia
     private String nombre;
@@ -46,80 +42,6 @@ public class ClubDeportivo implements gestionarMiembro {
         return entrenadores;
     }
 
-    // Método para registrar una persona en la lista
-    public void registrarPersona(Persona persona) {
-        validarPersonaExiste(persona);
-        listaPersonas.add(persona);
-    }
-
-    // Método para eliminar una persona en la lista
-    public void eliminarPersona(Persona persona) {
-        validarPersonaExiste(persona);
-        listaPersonas.remove(persona);
-    }
-
-    // Método para registrar un entrenador en la lista
-    public void registrarEntrenador(Entrenador entrenador) {
-        validarEntrenadorExiste(entrenador);
-        entrenadores.add(entrenador);
-    }
-
-    // Método para registrar un entrenador en la lista
-    public void eliminarEntrenador(Entrenador entrenador) {
-        validarEntrenadorExiste(entrenador);
-        entrenadores.remove(entrenador);
-    }
-
-    // Método que valida si la persona existe
-    private void validarPersonaExiste(Persona persona) {
-        boolean existePersona = buscarPersona(persona).isPresent();
-        AssertionUtil.ASSERTION.assertion(!existePersona, "La persona ya está registrada");
-    }
-
-    // Método que busca la persona en la lista por su nombre y numeroId
-    public Optional<Persona> buscarPersona(Persona persona) {
-        Predicate<Persona> nombreIgual = j -> j.getNombre().equals(persona.getNombre());
-        Predicate<Persona> numeroIdIgual = j -> j.getNumeroId().equals(persona.getNumeroId());
-        return listaPersonas.stream().filter(nombreIgual.and(numeroIdIgual)).findAny();
-    }
-
-    // Método que valida si el entrenador existe
-    private void validarEntrenadorExiste(Entrenador entrenador) {
-        boolean existePersona = buscarEntrenador(entrenador).isPresent();
-        AssertionUtil.ASSERTION.assertion(!existePersona, "El entrenador ya está registrado");
-    }
-
-    // Método que busca el entrenador en la lista por su nombre y deporte
-    public Optional<Entrenador> buscarEntrenador(Entrenador entrenador) {
-        Predicate<Entrenador> nombreIgual = j -> j.getNombre().equals(entrenador.getNombre());
-        Predicate<Entrenador> numeroIdIgual = j -> j.getDeporte().equals(entrenador.getDeporte());
-        return entrenadores.stream().filter(nombreIgual.and(numeroIdIgual)).findAny();
-    }
-
-    @Override
-    public void comprobarMiembroDeporte(Miembro miembro, Deporte deporte) {
-        switch (miembro.getTipoMiembro()) {
-            case JUVENIL:
-                switch (deporte.getNivelDificultad()) {
-                    case BAJO:
-                        deporte.registrarMiembro(miembro);
-                    case MEDIO:
-                        deporte.registrarMiembro(miembro);
-                        // System.out.println("Se ha inscrito exitosamente en un deporte de nivel");
-                        break;
-                    case ALTO:
-                        new RuntimeException("El miembro no se puede registrar debido al nivel de dificultad");
-                        // System.out.println("No puede inscribirse en deportes de nivel ALTO.");
-                        break;
-                }
-                break;
-
-            case ADULTO:
-                deporte.registrarMiembro(miembro);
-                // System.out.println("Se ha inscrito exitosamente en un deporte de nivel ");
-                break;
-
-        }
-    }
+    
 
 }
